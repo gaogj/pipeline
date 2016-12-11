533ddeafa37b8d6d0ca55c96d919039b0695f6fd
WCW <yubing744@163.com>
2016-12-11 13:48:55 +0800

    add other stages

---
+++ vars/integrationTesting.groovy
@@ -0,0 +1,16 @@
+// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+// method with the same name as the file.
+def call(stageName, body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    echo "stageName:${stageName}"
+    echo "param name:${config.name}"
+    echo "param version:${config.version}"
+    
+    stage(stageName) {
+	    echo "${stageName} run ok!"
+	}
+}
