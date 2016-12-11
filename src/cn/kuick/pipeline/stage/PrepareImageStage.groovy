16d6fdfa4b89a053fc04bc09dfa94c75aea32f15
WCW <yubing744@163.com>
2016-12-11 17:28:48 +0800

    ok

---
+++ src/cn/kuick/pipeline/stage/PrepareImageStage.groovy
@@ -0,0 +1,77 @@
+package cn.kuick.pipeline.stage;
+
+import java.io.Serializable;
+
+/**
+ *	准备镜像
+ */
+class PrepareImageStage implements Serializable {
+	def script;
+
+	def stageName;
+	def serverName;
+	def version;
+
+	PrepareImageStage(script, stageName, config) {
+		this.script = script;
+
+		this.stageName = stageName?stageName:"准备镜像";
+		this.serverName = config.name;
+		this.version = config.version;
+	}
+
+	def start() {
+		this.script.stage(this.stageName) {
+		    this.script.node('aliyun345-test') {
+		    	this.script.checkout this.script.scm
+
+		        this.run();
+		    }
+		}
+	}
+
+	def buildBase() {
+		def name = this.serverName;
+		def docker = this.script.docker;
+		def baseImage = docker.image("registry.kuick.cn/cc/${name}-server:base");
+
+		try {
+			baseImage.pull();
+		} catch(e) {
+			baseImage = docker.build("registry.kuick.cn/cc/${name}-server:base", '.');
+			baseImage.push();
+		}
+
+		return baseImage;
+	}
+
+	def buildTestBase() {
+		def name = this.serverName;
+		def docker = this.script.docker;
+		def baseImage = docker.image("registry.kuick.cn/cc/${name}-tester:base");
+
+		try {
+			baseImage.pull();
+		} catch(e) {
+			baseImage = docker.build("registry.kuick.cn/cc/${name}-tester:base", '-f ./release/docker/testBase.docker .');
+			baseImage.push();
+		}
+
+		return baseImage;
+	}
+
+	def run() {
+		def docker = this.script.docker;
+
+		// We are pushing to a private secure Docker registry in this demo.
+		// 'docker-registry-login' is the username/password credentials ID as defined in Jenkins Credentials.
+		// This is used to authenticate the Docker client to the registry.
+		docker.withRegistry('https://registry.kuick.cn', 'kuick_docker_registry_login') {
+			// Build base image
+			this.buildBase();
+
+			// Build TestBase image
+			this.buildTestBase();
+		}
+	}
+}
---
+++ src/cn/kuick/pipeline/stage/UnitTestStage.groovy
@@ -0,0 +1,61 @@
+package cn.kuick.pipeline.stage;
+
+import java.io.Serializable;
+
+/**
+ *	单元测试 + 生成镜像
+ */
+class UnitTestStage implements Serializable {
+	def script;
+
+	def stageName;
+	def serverName;
+	def version;
+
+	UnitTestStage(script, config) {
+		this.script = script;
+
+		this.stageName = '单元测试 + 生成镜像';
+		this.serverName = config.name;
+		this.version = config.version;
+	}
+
+	def start() {
+		this.script.stage(this.stageName) {
+		    this.script.node('aliyun345-test') {
+		    	this.script.checkout this.script.scm
+
+		        this.run();
+		    }
+		}
+	}
+
+	def buildTester() {
+		def name = this.serverName;
+		def version = this.version;
+
+		this.script.docker.build("registry.kuick.cn/cc/${name}-tester:${version}", '-f ./release/docker/tester.docker .');
+	}
+
+	def buildRelease() {
+		def name = this.serverName;
+		def version = this.version;
+
+		this.script.docker.build("registry.kuick.cn/cc/${name}-server:${version}", '-f ./release/docker/Dockerfile .');
+	}
+
+	def run() {
+		def docker = this.script.docker;
+
+		// We are pushing to a private secure Docker registry in this demo.
+		// 'docker-registry-login' is the username/password credentials ID as defined in Jenkins Credentials.
+		// This is used to authenticate the Docker client to the registry.
+		docker.withRegistry('https://registry.kuick.cn', 'kuick_docker_registry_login') {
+			// Build tester image
+			this.buildTester();
+
+			// Build release image
+			this.buildRelease();
+		}
+	}
+}
