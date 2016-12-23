87cef773e6069c708396c2a97dae3a757892d814
digimobistudio <digimobistudio@gmail.com>
2016-12-23 16:03:47 +0800

    添加接口测试

---
+++ src/cn/kuick/pipeline/stage/InterfaceTestStage.groovy
@@ -0,0 +1,57 @@
+package cn.kuick.pipeline.stage;
+
+import java.io.Serializable;
+
+/**
+ *	接口测试
+ */
+class InterfaceTestStage implements Serializable {
+	def script;
+
+	def stageName;
+	def serverName;
+	def version;
+
+	InterfaceTestStage(script, stageName, config) {
+		this.script = script;
+
+		this.stageName = stageName;
+		this.serverName = config.name;
+		this.version = config.version;
+	}
+
+	def start() {
+		this.script.stage(this.stageName) {
+		    this.script.node('aliyun345-test') {
+		    	this.script.checkout this.script.scm
+
+		       	this.run();
+		    }
+		}
+	}
+
+	def imageRun(image, args, command) {
+		def docker = this.script.docker;
+		def id = image.id;
+
+		docker.node {
+            def container = docker.script.sh(script: "docker run ${args != '' ? ' ' + args : ''} ${id}${command != '' ? ' ' + command : ''}", returnStdout: true).trim()
+            docker.script.dockerFingerprintRun containerId: container, toolName: docker.script.env.DOCKER_TOOL_NAME
+        }
+	}
+
+	def run() {
+		def name = this.serverName;
+		def version = this.version;
+		def docker = this.script.docker;
+
+		// We are pushing to a private secure Docker registry in this demo.
+		// 'docker-registry-login' is the username/password credentials ID as defined in Jenkins Credentials.
+		// This is used to authenticate the Docker client to the registry.
+		docker.withRegistry('https://registry.kuick.cn', 'kuick_docker_registry_login') {
+			// 运行接口测试
+			def testerImage = this.script.docker.image("registry.kuick.cn/cc/${name}-tester:${version}");
+			this.imageRun(testerImage, "", "pact_test");
+		}
+	}
+}
