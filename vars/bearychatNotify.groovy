76fbe4c71e22e106faff4d2ce174519fa3354549
WCW <yubing744@163.com>
2016-12-25 21:26:40 +0800

    fixbug for bearychat

--- vars/bearychatNotify.groovy
+++ vars/bearychatNotify.groovy
@@ -5,17 +5,19 @@ import groovy.json.JsonOutput;
 def call(text, channel = "技术讨论组") {
 	echo "start bearychat notify"
 
-	def slackURL = 'https://hook.bearychat.com/=bw9Bf/jenkins/ae0703c96e05dd7ffe4ae465c2081f18';
+	node("master") {
+		def slackURL = 'https://hook.bearychat.com/=bw9Bf/jenkins/ae0703c96e05dd7ffe4ae465c2081f18';
 
-    def payload = JsonOutput.toJson([
-    	text : text,
-        channel : channel,
-        username : "jenkins",
-        icon_emoji : ":jenkins:"
-    ])
+	    def payload = JsonOutput.toJson([
+	    	text : text,
+	        channel : channel,
+	        username : "jenkins",
+	        icon_emoji : ":jenkins:"
+	    ])
 
-    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
-
+	    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
+	}
+	
 	echo "end bearychat notify"
 }
 
