7a50194b0cda221bc06608b7006eb3d9800291ce
WCW <yubing744@163.com>
2016-12-25 21:17:36 +0800

    set default for channel

--- vars/bearychatNotify.groovy
+++ vars/bearychatNotify.groovy
@@ -2,14 +2,14 @@ import groovy.json.JsonOutput;
 
 // The call(body) method in any file in workflowLibs.git/vars is exposed as a
 // method with the same name as the file.
-def call(text, channel) {
+def call(text, channel = "技术讨论组") {
 	echo "start bearychat notify"
 
 	def slackURL = 'https://hook.bearychat.com/=bw9Bf/jenkins/ae0703c96e05dd7ffe4ae465c2081f18';
 
     def payload = JsonOutput.toJson([
     	text : text,
-        channel : channel?channel:"技术讨论组",
+        channel : channel,
         username : "jenkins",
         icon_emoji : ":jenkins:"
     ])
