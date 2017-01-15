a307f0077677c554d06495bb0b18a32f00b85db3
WCW <yubing744@163.com>
2017-01-15 19:52:22 +0800

    ok for prod deploy

--- src/cn/kuick/pipeline/stage/DeployProdStage.groovy
+++ src/cn/kuick/pipeline/stage/DeployProdStage.groovy
@@ -31,7 +31,7 @@ class DeployProdStage implements Serializable {
 				[$class: 'PasswordParameterDefinition', defaultValue: '', description: '部署正式服务器授权码', name: '授权码']
 			];
 
-			if (DEPLOY_TOKEN == token) {
+			if (DEPLOY_TOKEN.equals(token)) {
 		    	this.run();
 		    } else {
 		    	throw new RuntimeException("部署正式服务器授权码不正确！");
