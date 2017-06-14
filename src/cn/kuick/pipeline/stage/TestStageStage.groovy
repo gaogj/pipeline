9854618b8525335de05f9b0a058ac7680af5f1ef
Johny.Zheng <shun.johny@gmail.com>
2017-06-14 14:53:58 +0800

    测试stage

---
+++ src/cn/kuick/pipeline/stage/TestStageStage.groovy
@@ -0,0 +1,50 @@
+package cn.kuick.pipeline.stage;
+
+import java.io.Serializable;
+
+/**
+ *	部署测试环境
+ */
+
+ // This is test module 
+class TestStageStage implements Serializable {
+	def script;
+
+	def stageName;
+	def serverName;
+	def version;
+	def commitId
+
+	TestStageStage(script, stageName, config) {
+		this.script = script;
+
+		this.stageName = stageName;
+		this.serverName = config.name;
+		this.version = config.version;
+		this.commitId = config.commitId
+	}
+
+	def start() {
+		this.script.stage(this.stageName) {
+		    this.run();
+		}
+	}
+
+	def run() {
+		def version = this.version;
+
+		this.script.node('aliyun327-test') {
+			this.script.echo "login to aliyun327-test"
+
+	    	this.script.checkout this.script.scm
+
+	    	this.script.sh "git reset --hard ${commitId}"
+
+	    	this.script.sh "release/docker/pull.sh ${version}"
+	    	
+			this.script.sh "./release/docker/test/deploy.sh ${version}";
+
+			this.script.echo "deploy test success!"
+	    }
+	}
+}
