e6ee290e4f914c5a14102248a0ac442f6edcde3c
Johny.Zheng <shun.johny@gmail.com>
2017-06-17 23:49:36 +0800

    fix  can't build tester base image

--- src/cn/kuick/pipeline/stage/BuildBaseImageStage.groovy
+++ src/cn/kuick/pipeline/stage/BuildBaseImageStage.groovy
@@ -52,6 +52,7 @@ class BuildBaseImageStage implements Serializable {
 
 	def run() {
 		def docker = this.script.docker;
+		def TestBaseImage_exists = fileExists './release/docker/testBase.docker';
 
 		// We are pushing to a private secure Docker registry in this demo.
 		// 'docker-registry-login' is the username/password credentials ID as defined in Jenkins Credentials.
@@ -64,7 +65,11 @@ class BuildBaseImageStage implements Serializable {
 			this.buildBase();
 
 			// Build TestBase image
-			this.buildTestBase();
+			if (TestBaseImage_exists) {
+			    this.buildTestBase();}
+			else {
+                echo 'Don not have to  Build TestBase image'
+                }
 		}
 	}
 }
