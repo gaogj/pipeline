aca44366be947eb227e311c77041398d8c458666
Johny.Zheng <shun.johny@gmail.com>
2017-06-17 23:59:13 +0800

    fix typo

--- src/cn/kuick/pipeline/stage/BuildBaseImageStage.groovy
+++ src/cn/kuick/pipeline/stage/BuildBaseImageStage.groovy
@@ -52,7 +52,7 @@ class BuildBaseImageStage implements Serializable {
 
 	def run() {
 		def docker = this.script.docker;
-		def TestBaseImage_exists = fileExists 'release/docker/testBase.docker';
+		def TestBaseImageExists = fileExists 'release/docker/testBase.docker';
 
 		// We are pushing to a private secure Docker registry in this demo.
 		// 'docker-registry-login' is the username/password credentials ID as defined in Jenkins Credentials.
@@ -65,7 +65,7 @@ class BuildBaseImageStage implements Serializable {
 			this.buildBase();
 
 			// Build TestBase image
-			if (TestBaseImage_exists) {
+			if (TestBaseImageExists ) {
 			    this.buildTestBase();}
 			else {
                 echo 'Don not have to  Build TestBase image'
