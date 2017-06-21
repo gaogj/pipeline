e8f038f3ad378694f6dbbaf14b8b8c285f79299d
Johny.Zheng <shun.johny@gmail.com>
2017-06-21 13:41:51 +0800

    自动合并分支

---
+++ vars/postDeployAutoMerge.groovy
@@ -0,0 +1,14 @@
+// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+import cn.kuick.pipeline.stage.PostDeployAutoMergeStage
+
+// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+// method with the same name as the file.
+def call(stageName, body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def stage = new PostDeployAutoMergeStage(this, stageName, config);
+    stage.start();
+}
