0eda5806b91899b440e0989ad31330f562fc81f1
Johny.Zheng <shun.johny@gmail.com>
2017-08-29 17:53:21 +0800

    Fix auto clean merged branch

--- src/cn/kuick/pipeline/stage/PostDeployAutoMergeStage.groovy
+++ src/cn/kuick/pipeline/stage/PostDeployAutoMergeStage.groovy
@@ -84,9 +84,9 @@ class PostDeployAutoMergeStage implements Serializable {
 
 		    this.script.sh "The Follow branch will be deleted!"
 
-		    this.script.sh "git branch -r --merged develop| grep -v develop|grep -v master|grep -v HEAD|sed 's/origin"\"///'"
+		    this.script.sh "git branch -r --merged develop| grep -v develop|grep -v master|grep -v HEAD| cut -d/ -f2-
 
-		    this.script.sh "git branch -r --merged develop| grep -v develop|grep -v master|grep -v HEAD|sed 's/origin"\"///' | xargs -n 1 git push --delete origin"
+		    this.script.sh "git branch -r --merged develop| grep -v develop|grep -v master|grep -v HEAD| cut -d/ -f2- | xargs -n 1 git push --delete origin"
 
 		    this.script.echo "Clean branch which merged to develop success!"
 
