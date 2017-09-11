1a06d68a50856c43704d309626589cf5b73fb6c4
Johny.Zheng <shun.johny@gmail.com>
2017-09-11 16:23:37 +0800

    fix

--- src/cn/kuick/pipeline/stage/PostDeployAutoMergeStage.groovy
+++ src/cn/kuick/pipeline/stage/PostDeployAutoMergeStage.groovy
@@ -82,9 +82,9 @@ class PostDeployAutoMergeStage implements Serializable {
 
 		    // 清理已经合并的分支
 
-		    this.script.sh "cd shared/scripts && pwd && ls -la"
+		    this.script.sh "./shared/scripts/clean_merged_branch.sh";
 
-		    this.script.sh "clean_merged_branch.sh"
+			this.script.sh "./release/docker/clean_merged_branch.sh";
 
 		    this.script.echo "Clean branch which merged to develop success!"
 
