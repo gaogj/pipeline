617f5f5516e43a1a772151d57cd56206815b0a06
Johny.Zheng <shun.johny@gmail.com>
2017-09-20 17:20:15 +0800

    Revert "fix"
    This reverts commit 7a5c45b87e0000604d4ccb6facf5b1773a4e6ff7.

--- src/cn/kuick/pipeline/stage/UnitTestStage.groovy
+++ src/cn/kuick/pipeline/stage/UnitTestStage.groovy
@@ -23,7 +23,7 @@ class UnitTestStage implements Serializable {
 	def start() {
 		this.script.stage(this.stageName) {
 		    this.script.node('aliyun345-test') {
-		    	// this.script.checkout this.script.scm
+		    	this.script.checkout this.script.scm
 
 		       	this.run();
 		    }
