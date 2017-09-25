9eb3f025debadc10b368bf9e0c7a34ccd77a8fa4
Johny.Zheng <shun.johny@gmail.com>
2017-09-25 19:41:19 +0800

    部署新vpc环境 临时不打tag

--- vars/test3ApiTesting.groovy
+++ vars/test3ApiTesting.groovy
@@ -1,3 +1,25 @@
+//// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+//// method with the same name as the file.
+//def call(stageName, body) {
+//    def config = [:]
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//    body()
+//
+//    echo "stageName:${stageName}"
+//    echo "param name:${config.name}"
+//    echo "param version:${config.version}"
+//
+//    stage(stageName) {
+//	    echo "${stageName} run ok!"
+//	}
+//}
+
+// 迁移完成完成启用接口测试
+
+// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+import cn.kuick.pipeline.stage.DeployProdVPCStage
+
 // The call(body) method in any file in workflowLibs.git/vars is exposed as a
 // method with the same name as the file.
 def call(stageName, body) {
@@ -6,11 +28,6 @@ def call(stageName, body) {
     body.delegate = config
     body()
 
-    echo "stageName:${stageName}"
-    echo "param name:${config.name}"
-    echo "param version:${config.version}"
-
-    stage(stageName) {
-	    echo "${stageName} run ok!"
-	}
+    def stage = new DeployProdVPCStage(this, stageName, config);
+    stage.start();
 }
