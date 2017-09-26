ee7caa289803556f42a200706f5d782b67cc1c53
Johny.Zheng <shun.johny@gmail.com>
2017-09-26 23:13:18 +0800

    build vpc

--- vars/deployTest.groovy
+++ vars/deployTest.groovy
@@ -1,14 +1,31 @@
+//// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+//import cn.kuick.pipeline.stage.DeployTestStage
+//
+//// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+//// method with the same name as the file.
+//def call(stageName, body) {
+//    def config = [:]
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//    body()
+//
+//    def stage = new DeployTestStage(this, stageName, config);
+//    stage.start();
+//}
+
+// 迁移完成完成删除下面
+
 // See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
-import cn.kuick.pipeline.stage.DeployTestStage
+import cn.kuick.pipeline.stage.DeployProdVPCStage
 
 // The call(body) method in any file in workflowLibs.git/vars is exposed as a
 // method with the same name as the file.
-def call(stageName, body) {
+def callvpc(stageName, body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
-    def stage = new DeployTestStage(this, stageName, config);
+    def stage = new DeployProdVPCStage(this, stageName, config);
     stage.start();
 }
