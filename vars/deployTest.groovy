bf8b0ea0f08bb5b5d6c0dcda664f0f16e5e01d25
Johny.Zheng <shun.johny@gmail.com>
2017-09-27 02:56:35 +0800

    recover test1

--- vars/deployTest.groovy
+++ vars/deployTest.groovy
@@ -1,22 +1,5 @@
-//// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
-//import cn.kuick.pipeline.stage.DeployTestStage
-//
-//// The call(body) method in any file in workflowLibs.git/vars is exposed as a
-//// method with the same name as the file.
-//def call(stageName, body) {
-//    def config = [:]
-//    body.resolveStrategy = Closure.DELEGATE_FIRST
-//    body.delegate = config
-//    body()
-//
-//    def stage = new DeployTestStage(this, stageName, config);
-//    stage.start();
-//}
-
-// 迁移完成完成删除下面
-
 // See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
-import cn.kuick.pipeline.stage.DeployProdVPCStage
+import cn.kuick.pipeline.stage.DeployTestStage
 
 // The call(body) method in any file in workflowLibs.git/vars is exposed as a
 // method with the same name as the file.
@@ -26,6 +9,23 @@ def call(stageName, body) {
     body.delegate = config
     body()
 
-    def stage = new DeployProdVPCStage(this, stageName, config);
+    def stage = new DeployTestStage(this, stageName, config);
     stage.start();
 }
+
+//// 迁移完成完成删除下面
+//
+//// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+//import cn.kuick.pipeline.stage.DeployProdVPCStage
+//
+//// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+//// method with the same name as the file.
+//def call(stageName, body) {
+//    def config = [:]
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//    body()
+//
+//    def stage = new DeployProdVPCStage(this, stageName, config);
+//    stage.start();
+//}
