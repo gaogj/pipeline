96f25322fda9d078af8bbcdb4d50565251cfe368
Johny.Zheng <shun.johny@gmail.com>
2017-09-26 23:15:27 +0800

    build vpc

--- vars/deployProd.groovy
+++ vars/deployProd.groovy
@@ -20,7 +20,7 @@ def call(stageName, body) {
 //
 //// The call(body) method in any file in workflowLibs.git/vars is exposed as a
 //// method with the same name as the file.
-//def callvpc(stageName, body) {
+//def call(stageName, body) {
 //    def config = [:]
 //    body.resolveStrategy = Closure.DELEGATE_FIRST
 //    body.delegate = config
--- vars/deployTest.groovy
+++ vars/deployTest.groovy
@@ -20,7 +20,7 @@ import cn.kuick.pipeline.stage.DeployProdVPCStage
 
 // The call(body) method in any file in workflowLibs.git/vars is exposed as a
 // method with the same name as the file.
-def callvpc(stageName, body) {
+def call(stageName, body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
