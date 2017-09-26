925f4ff19bb05c3fb58a8ddb03784f52a99b52ef
Johny.Zheng <shun.johny@gmail.com>
2017-09-26 17:26:01 +0800

    test

--- vars/deployProd.groovy
+++ vars/deployProd.groovy
@@ -1,19 +1,19 @@
-//// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
-//import cn.kuick.pipeline.stage.DeployProdStage
-//
-//// The call(body) method in any file in workflowLibs.git/vars is exposed as a
-//// method with the same name as the file.
-//def call(stageName, body) {
-//    def config = [:]
-//    body.resolveStrategy = Closure.DELEGATE_FIRST
-//    body.delegate = config
-//    body()
-//
-//    def stage = new DeployProdStage(this, stageName, config);
-//    stage.start();
-//}
+// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+import cn.kuick.pipeline.stage.DeployProdStage
 
-// 迁移完成完成启用
+// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+// method with the same name as the file.
+def call(stageName, body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def stage = new DeployProdStage(this, stageName, config);
+    stage.start();
+}
+
+// 迁移完成完成删除下面
 
 // See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
 import cn.kuick.pipeline.stage.DeployProdVPCStage
