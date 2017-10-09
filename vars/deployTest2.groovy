65a3c65a0d356c7942802faba01aa6c1813f8bd3
Johny.Zheng <shun.johny@gmail.com>
2017-10-09 11:42:57 +0800

    切换到部署新环境

--- vars/deployTest2.groovy
+++ vars/deployTest2.groovy
@@ -13,19 +13,4 @@ def call(stageName, body) {
     stage.start();
 }
 
-// 迁移完成完成删除下面
 
-//// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
-//import cn.kuick.pipeline.stage.DeployProdVPCStage
-//
-//// The call(body) method in any file in workflowLibs.git/vars is exposed as a
-//// method with the same name as the file.
-//def call(stageName, body) {
-//    def config = [:]
-//    body.resolveStrategy = Closure.DELEGATE_FIRST
-//    body.delegate = config
-//    body()
-//
-//    def stage = new DeployProdVPCStage(this, stageName, config);
-//    stage.start();
-//}
