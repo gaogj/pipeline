296d9c1ad072431f9b444681208c711846cf996d
Johny.Zheng <shun.johny@gmail.com>
2017-12-05 16:52:06 +0800

    test

--- src/cn/kuick/pipeline/stage/SonarQubeStage.groovy
+++ src/cn/kuick/pipeline/stage/SonarQubeStage.groovy
@@ -43,7 +43,7 @@ class SonarQubeStage implements Serializable {
 
         //withSonarQubeEnv('SonarQube') {
             //this.script.sh "${sonarqubeScannerHome}/bin/sonar-scanner"
-            this.script.bash "source  /etc/profile && ./gradlew --info sonarqube"
+            this.script.sh "/usr/bin/bash && source  /etc/profile && ./gradlew --info sonarqube"
             //}
 
         }
