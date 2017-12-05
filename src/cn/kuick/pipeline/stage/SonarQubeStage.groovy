5e711bb4bd264adbff107b6104b85df21810f10e
Johny.Zheng <shun.johny@gmail.com>
2017-12-05 17:22:36 +0800

    test

--- src/cn/kuick/pipeline/stage/SonarQubeStage.groovy
+++ src/cn/kuick/pipeline/stage/SonarQubeStage.groovy
@@ -42,6 +42,8 @@ class SonarQubeStage implements Serializable {
 	        this.script.checkout this.script.scm
 
             this.script.sh "./gradlew sonarqube   -Dsonar.host.url=https://sonar.kuick.cn   -Dsonar.login=74a5055a367c4a64bcb5d1a136690126a78a1510"
+            
+            this.script.echo "Please login and check your code :https://sonar.kuick.cn/projects"
 
         }
 
