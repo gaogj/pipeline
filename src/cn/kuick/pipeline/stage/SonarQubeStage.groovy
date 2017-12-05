376ca07ff76118026d8eb3be178c011e7c666cfe
Johny.Zheng <shun.johny@gmail.com>
2017-12-05 15:20:26 +0800

    feat: 加入 sonar

---
+++ src/cn/kuick/pipeline/stage/SonarQubeStage.groovy
@@ -0,0 +1,45 @@
+package cn.kuick.pipeline.stage;
+
+import java.io.Serializable;
+
+/**
+ *	SonarQube analysis
+ */
+class SonarQubeStage implements Serializable {
+	def script;
+
+	def stageName;
+	def serverName;
+	def version;
+
+	SonarQubeStage(script, stageName, config) {
+		this.script = script;
+
+		this.stageName = stageName;
+		this.serverName = config.name;
+		this.version = config.version;
+	}
+
+	def start() {
+		this.script.stage(this.stageName) {
+		    this.script.node('aliyun345-test') {
+		    	this.script.checkout this.script.scm
+
+		       	this.run();
+		    }
+		}
+	}
+
+	def run() {
+		def version = this.version;
+		def docker = this.script.docker;
+	    def sonarqubeScannerHome = tool name: 'SonarQube Scanner';
+
+        withSonarQubeEnv('SonarQube') {
+            sh "${sonarqubeScannerHome}/bin/sonar-scanner"
+
+        }
+
+	}
+
+}
