3645720cc37fffd03dc34fe6b7b490764c67ea21
Johny.Zheng <shun.johny@gmail.com>
2018-05-31 16:37:43 +0800

    no message

--- src/cn/kuick/pipeline/stage/DeployProdVPCStage.groovy
+++ src/cn/kuick/pipeline/stage/DeployProdVPCStage.groovy
@@ -2,18 +2,20 @@ package cn.kuick.pipeline.stage;
 
 import java.io.Serializable;
 
-//@NonCPS
-//def getBuildUser() {
-//	def cause = currentBuild.rawBuild.getCause(Cause.UserIdCause);
-//
-//	if (cause != null) {
-//		return cause.getUserId()
-//	}
-//
-//	return "gitlab"
-//}
-//
-//userId = this.getBuildUser();
+@NonCPS
+def getBuildUser() {
+	def cause = currentBuild.rawBuild.getCause(Cause.UserIdCause);
+
+	if (cause != null) {
+		return cause.getUserId()
+	}
+
+	return "gitlab"
+}
+
+userId = this.getBuildUser();
+
+echo "USER_ID:${userId}"
 
 /**
  *	部署正式环境 + 自动打tag
