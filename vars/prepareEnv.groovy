6bb1bfae3175895d4bb6f5733aa2f3da704dda08
Administrator <admin@example.com>
2018-05-31 12:01:15 +0800

    fix: merge

--- vars/prepareEnv.groovy
+++ vars/prepareEnv.groovy
@@ -22,6 +22,8 @@ def printPipelineEnv() {
 	echo "CHANGE_AUTHOR_EMAIL:${env.CHANGE_AUTHOR_EMAIL}"
 	echo "CHANGE_TARGET:${env.CHANGE_TARGET}"
 
+    echo "USER_ID:${env.USER_ID}"
+
 	echo "------------------printPipelineEnv-end----------------"
 	echo "----------------------------------------------"
 }
@@ -61,6 +63,11 @@ def printGitLabEnv() {
 
 	echo "------------------printGitLabEnv-end----------------"
 	echo "----------------------------------------------"
+}
+
+@NonCPS
+def getBuildUser() {
+    return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
 }
 
 def call(envCallback) {
@@ -112,6 +119,14 @@ def call(envCallback) {
 	if (env.gitlabTargetBranch != null) {
 		env.CHANGE_TARGET = env.gitlabTargetBranch;
 	}
+
+	// CHANGE_TARGET
+	if (env.gitlabTargetBranch != null) {
+		env.CHANGE_TARGET = env.gitlabTargetBranch;
+	}
+    
+	// USER ID
+	env.USER_ID = this.getBuildUser();
 
 	this.printPipelineEnv();
 }
