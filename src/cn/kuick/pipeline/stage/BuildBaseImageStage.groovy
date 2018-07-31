18a0b299b169c6fb0a6e4b20454365ec95eeecc2
Johny.Zheng <shun.johny@gmail.com>
2018-07-31 19:21:55 +0800

    base nocache

--- src/cn/kuick/pipeline/stage/BuildBaseImageStage.groovy
+++ src/cn/kuick/pipeline/stage/BuildBaseImageStage.groovy
@@ -47,7 +47,7 @@ class BuildBaseImageStage implements Serializable {
 		def name = this.serverName;
 		def docker = this.script.docker;
 
-		def baseImage = docker.build("cc/${name}-tester:base", '-f ./release/docker/testBase.docker .');
+		def baseImage = docker.build("cc/${name}-tester:base", '-f ./release/docker/testBase.docker . --no-cache');
 		baseImage.push();
 
 		return baseImage;
