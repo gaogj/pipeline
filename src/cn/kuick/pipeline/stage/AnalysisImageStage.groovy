792bc804e198669942f16750d89699292a025c87
guangjun gao <gaogj@219.1.168.188.retail.ttk.ru>
2018-09-10 12:58:02 +0800

    add send report mail

--- src/cn/kuick/pipeline/stage/AnalysisImageStage.groovy
+++ src/cn/kuick/pipeline/stage/AnalysisImageStage.groovy
@@ -46,7 +46,57 @@ class AnalysisImageStage implements Serializable {
 
         def parameter = "--ip='${buildNodeIP}' --clair='${clairUrl}' --report=${reportPath} ${imageName} "
 
-        this.script.sh "clair-scanner ${parameter}"
+        try {
+            this.script.sh "clair-scanner ${parameter}"
+
+            def buildId = env.BUILD_ID;
+            def toMail = env.gitlabUserEmail;
+
+            echo "start send fail mail!"
+
+            this.script.mail([
+                    bcc: '',
+                    body: "${env.gitlabSourceRepoName} 镜像扫描结果 At buildId(#${buildId})",
+                    cc: 'devops@kuick.cn',
+                    from: 'jenkins2@kuick.cn',
+                    replyTo: '',
+                    subject: "附件为镜像漏洞扫描结果",
+                    attachmentsPattern: reportPath,
+                    to: toMail
+            ]);
+
+            echo "fail mail send ok!"
+
+            bearychatNotify("${env.gitlabSourceRepoName} Pull Request failed for buildId(#${buildId})");
+
+            throw e;
+
+        } catch(e){
+            def buildId = env.BUILD_ID;
+            def toMail = env.gitlabUserEmail;
+
+            echo "start send fail mail!"
+
+            this.script.mail([
+                    bcc: '',
+                    body: "${env.gitlabSourceRepoName} 镜像漏洞扫描失败 At buildId(#${buildId})",
+                    cc: 'devops@kuick.cn',
+                    from: 'jenkins2@kuick.cn',
+                    replyTo: '',
+                    subject: "${env.gitlabSourceRepoName} 镜像漏洞扫描失败 at " + buildId,
+                    to: toMail
+            ]);
+
+            echo "fail mail send ok!"
+
+            bearychatNotify("${env.gitlabSourceRepoName} Pull Request failed for buildId(#${buildId})");
+
+            throw e;
+
+
+        }
+
+
     }
 }
 
