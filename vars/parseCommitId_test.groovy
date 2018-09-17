52b1501b5f333f1711e34dd7548190ea08c37b21
zhoubangjun <zhoubangjun@kuick.cn>
2018-09-17 21:15:42 +0800

    修改tag的获取方式

--- vars/parseCommitId_test.groovy
+++ vars/parseCommitId_test.groovy
@@ -5,7 +5,9 @@ def call() {
 
 		def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
 		def shortCommit = gitCommit.take(6)
-		def lastTagId = sh(returnStdout: true, script: 'git tag --sort=committerdate | tail -1').trim()
+		// def lastTagId = sh(returnStdout: true, script: 'git tag --sort=committerdate | tail -1').trim()
+		// jenkins 不支持 --sort=committerdate
+		def lastTagId = sh(returnStdout: true, script: 'git describe --tags `git rev-list --tags --max-count=1`  2>/dev/null || echo noTags').trim()
 
 		env.COMMIT_ID = shortCommit;
 		env.LAST_TAG_ID = lastTagId
