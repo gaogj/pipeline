8714567a1d4d89ae30504e0b4b8cec5b5424ad6a
zhoubangjun <zhoubangjun@kuick.cn>
2018-09-17 14:42:49 +0800

    测试备份与回滚

---
+++ src/cn/kuick/pipeline/stage/PostDeployAutoChangeLogStage_test.groovy
@@ -0,0 +1,77 @@
+package cn.kuick.pipeline.stage;
+
+import java.io.Serializable;
+
+/**
+ *	自动更新changelog
+ */
+class PostDeployAutoChangeLogStage implements Serializable {
+	def script;
+
+	def stageName;
+	def serverName;
+	def version;
+	def commitId;
+	def branch;
+	def changeLog;
+
+
+	PostDeployAutoChangeLogStage(script, stageName, config) {
+		this.script = script;
+
+		this.stageName = stageName;
+		this.serverName = config.name;
+		this.version = config.version;
+		this.commitId = version[-6..-1];
+		this.branch = config.branch;
+		this.changeLog = config.changeLog;
+
+	}
+
+	def start() {
+		this.script.stage(this.stageName) {
+		    this.run345();
+		}
+	}
+
+	def run345() {
+		def version = this.version;
+
+
+		this.script.node('aliyun327-test') {
+			this.script.echo "login to aliyun327-test"
+
+	    	this.script.checkout this.script.scm
+
+	        this.script.sh "git fetch"
+
+	        this.script.sh "git checkout -B ${branch} --track origin/${branch} "
+
+            // Update changelog
+            if (changeLog == "Y") {
+
+                this.script.sh "npm install --registry=https://registry.npm.taobao.org"
+
+               // this.script.sh "npm install commitizen@2.3.0 validate-commit-msg@2.11.1 conventional-changelog-cli@1.2.0 husky@0.13.1  --registry=https://registry.npm.taobao.org"
+
+                this.script.sh "npm run changelog"
+
+                this.script.sh "git add CHANGELOG.md backupVersion.txt"
+
+                this.script.sh "git commit -m 'docs: Update Changelog commit ${commitId}'"
+
+	            this.script.sh "git push origin ${branch}"
+
+                this.script.echo "Success Update Changelog on ${branch}!!!"
+
+                }
+            else {
+                this.script.echo "Skipped Update Changelog!!!"
+                }
+
+
+	    }
+
+}
+
+}
---
+++ src/cn/kuick/pipeline/stage/RollbackStage.groovy
@@ -0,0 +1,82 @@
+package cn.kuick.pipeline.stage;
+
+import java.io.Serializable;
+
+/**
+ *	回滚服务到上一个镜像版本
+ */
+class RollbackStage implements Serializable {
+	static String DEPLOY_TOKEN = "cc123456v5";
+	
+	def script;
+
+	def stageName;
+	def serverName;
+	def lastVersion;
+	def deployNode;
+	def commitId;
+
+	RollbackStage(script, stageName, config) {
+		this.script = script;
+
+		this.stageName = stageName;
+		this.serverName = config.name;
+		this.lastVersion = config.lastVersion;
+		this.deployNode = config.deployNode;
+	}
+
+	def start() {
+		this.script.stage(this.stageName) {
+		    this.run();
+		}
+	}
+
+	def readProperties(propFile) {
+		return this.script.readProperties([file: propFile])
+	}
+
+	def run() {
+		def lastVersion = this.lastVersion;
+		def deployNode = this.deployNode;
+
+		// 回滚服务
+		this.script.node("aliyun327-test") {
+	        this.script.echo "login to aliyun327-test"
+
+	        this.script.checkout this.script.scm
+
+	        def serverEnv = [];
+
+	        this.script.dir("deploy-config") {
+
+	            // application.properties
+	            def properties = this.readProperties("prod/aliyuncsvpc/application.properties");
+
+	            for(def entry : properties) {
+	            	def key = entry.key.trim().replace(".", "_").toUpperCase();
+	            	def value = entry.value.trim();
+
+	            	def item = "${key}=${value}";
+	            	serverEnv.add(item)
+	            }
+
+	            // certs
+	            def PGRDIR = this.script.pwd();
+
+	           	serverEnv.add("DOCKER_TLS_VERIFY=1")
+				serverEnv.add("DOCKER_HOST=tcp://master3g9.cs-cn-hangzhou.aliyun.com:20103")
+				serverEnv.add("DOCKER_CERT_PATH=$PGRDIR/prod/aliyuncs/certs")
+	        }
+
+	        this.script.withEnv(serverEnv) {
+
+	        	// 回滚prod
+	            this.script.sh "release/docker/test/deploy.sh ${lastVersion}"
+	            this.script.sh "echo 'rollback' >> backupVersion.txt"
+
+	        }
+
+	        this.script.echo "rollback prod success!"
+	    }
+	}
+}
