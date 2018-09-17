9dc1e4e3146c2b3687259221304ccbd4a62e2a2d
zhoubangjun <zhoubangjun@kuick.cn>
2018-09-17 15:31:33 +0800

    修复取返回值

--- vars/parseCommitId_test.groovy
+++ vars/parseCommitId_test.groovy
@@ -5,7 +5,7 @@ def call() {
 
 		def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
 		def shortCommit = gitCommit.take(6)
-		def lastTagId = sh(git tag --sort=committerdate | tail -1)
+		def lastTagId = sh(returnStdout: true, script: 'git tag --sort=committerdate | tail -1').trim()
 
 		env.COMMIT_ID = shortCommit;
 		env.LAST_TAG_ID = lastTagId
