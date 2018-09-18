7dd078a2a79da962bf1ae81976576496a98a9e7c
zhoubangjun <zhoubangjun@kuick.cn>
2018-09-18 18:58:21 +0800

    导入遗漏的模块

--- vars/backupVersion.groovy
+++ vars/backupVersion.groovy
@@ -1,3 +1,6 @@
+// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+import cn.kuick.pipeline.stage.BackupVersionStage
+
 // The call(body) method in any file in workflowLibs.git/vars is exposed as a
 // method with the same name as the file.
 def call(stageName, body) {
