99c64d8f927771cded7cd1e972f6f46cca838aa7
test <test>
2018-09-20 15:50:54 +0800

    test new jenkinsfile

--- vars/configure.groovy
+++ vars/configure.groovy
@@ -149,18 +149,18 @@ class Tasks implements Serializable {
     }
 }
 
-public class Person {
-    private String name;
-    Person(String name) {
-        this.name = name
-    }
-    public String getName() {
-        return name
-    }
-    public void setName(String name) {
-        this.name = name
-    }
-}
+// public class Person {
+//     private String name;
+//     Person(String name) {
+//         this.name = name
+//     }
+//     public String getName() {
+//         return name
+//     }
+//     public void setName(String name) {
+//         this.name = name
+//     }
+// }
 
 // In Groovy
 class Person {
