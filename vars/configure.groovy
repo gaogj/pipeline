7671b495799c2472e5dc1de682836c9fd63f3a9f
test <test>
2018-09-20 20:05:01 +0800

    添加直接部署和REBASE

--- vars/configure.groovy
+++ vars/configure.groovy
@@ -18,6 +18,8 @@ import cn.kuick.pipeline.stage.Test3ApiTestingStage
 import cn.kuick.pipeline.stage.SmokeTestingStage
 import cn.kuick.pipeline.stage.PostDeployAutoChangeLogStage
 import cn.kuick.pipeline.stage.PostDeployAutoMergeStage
+import cn.kuick.pipeline.stage.BuildBaseImageStage
+
 
 
 class Tasks implements Serializable {
@@ -69,14 +71,27 @@ class Tasks implements Serializable {
         //TODO send email
     }
 
-    def DeployToTest(skip) {
+    def DeployToTest1(skip) {
         //
-        if (skip == true) {
+        if (skip) {
             // 跳过以下步骤，保持视图完整
             this.script.stage("部署测试服务器") {
                 this.script.echo 'Skipped'
+                }
             }
 
+        }else{
+            //
+            def DeployTest = new DeployTestStage(this.script,'部署测试服务器',this.config)
+            DeployTest.start();
+        }
+
+    }
+
+
+        def DeployToTest2() {
+        //
+        if (skip) {
             this.script.stage("确认部署测试2") {
                 this.script.echo 'Skipped'
             }
@@ -95,27 +110,30 @@ class Tasks implements Serializable {
 
         }else{
             //
-            def DeployTest = new DeployTestStage(this.script,'部署测试服务器',this.config)
-            DeployTest.start();
-
-            this.config.tips = '该服务是否可以部署测试2?'
-            this.config.timeout = 12
-            this.config.timeoutUnit = 'HOURS'
-            def DeployTest2Messger = new ConfirmMessgerStage(this.script,'确认部署测试2',this.config)
-            DeployTest2Messger.start()
+            if (this.script.env.CHANGE_TARGET != "DEPLOY_TEST2"){
+            //如果是直接部署test2跳过提示
+                this.config.tips = '该服务是否可以部署测试2?'
+                this.config.timeout = 12
+                this.config.timeoutUnit = 'HOURS'
+                def DeployTest2Messger = new ConfirmMessgerStage(this.script,'确认部署测试2',this.config)
+                DeployTest2Messger.start()
+            }
 
             def DeployTest2 = new DeployTest2Stage(this.script,'部署测试2服务器',this.config)
             DeployTest2.start()
 
-            this.script.stage("测试2 API接口测试") {
-                this.script.echo 'Skipped'
-            }
+            if (this.script.env.CHANGE_TARGET != "DEPLOY_TEST2"){
+                //如果是直接部署test2跳过提示
+                this.script.stage("测试2 API接口测试") {
+                    this.script.echo 'Skipped'
+                }
 
-            this.config.tips = 'QA测试是否通过??'
-            this.config.timeout = 12
-            this.config.timeoutUnit = 'HOURS'
-            def QATestMessger = new ConfirmMessgerStage(this.script,'QA测试',this.config)
-            QATestMessger.start()
+                this.config.tips = 'QA测试是否通过??'
+                this.config.timeout = 12
+                this.config.timeoutUnit = 'HOURS'
+                def QATestMessger = new ConfirmMessgerStage(this.script,'QA测试',this.config)
+                QATestMessger.start()
+            }
         }
 
     }
@@ -123,45 +141,56 @@ class Tasks implements Serializable {
 
     def DeployToTest3() {
         //
-        this.config.tips = '该服务是否可以部署测试3?'
-        this.config.timeout = 24
-        this.config.timeoutUnit = 'HOURS'
-        def DeployToTest3Messger = new ConfirmMessgerStage(this.script,'确认部署测试3',this.config)
-        DeployToTest3Messger.start()
+        if (this.script.env.CHANGE_TARGET != "DEPLOY_TEST3"){
+        //如果是直接部署test3 跳过提示
+            this.config.tips = '该服务是否可以部署测试3?'
+            this.config.timeout = 24
+            this.config.timeoutUnit = 'HOURS'
+            def DeployToTest3Messger = new ConfirmMessgerStage(this.script,'确认部署测试3',this.config)
+            DeployToTest3Messger.start()
+        }
         //
         def DeployTest3 = new DeployTest3Stage(this.script,'部署测试3服务器',this.config)
         DeployTest3.start()
-        // 
+
         def Test3ApiTesting = new Test3ApiTestingStage(this.script,'测试3 API接口测试',this.config)
         Test3ApiTesting.start()
+        
+        if (this.script.env.CHANGE_TARGET != "DEPLOY_TEST3"){
+        //如果是直接部署test3 跳过提示
+            this.script.stage('回归测试') {
+                this.script.echo "Skipped"
+            }
+            //
+            this.config.tips = '验收测试是否通过?'
+            this.config.timeout = 12
+            this.config.timeoutUnit = 'HOURS'
+            def QATestMessger = new ConfirmMessgerStage(this.script,'验收测试',this.config)
+            QATestMessger.start()
+            }
         //
-        this.script.stage('回归测试') {
-            this.script.echo "Skipped"
         }
-        //
-        this.config.tips = '验收测试是否通过?'
-        this.config.timeout = 12
-        this.config.timeoutUnit = 'HOURS'
-        def QATestMessger = new ConfirmMessgerStage(this.script,'验收测试',this.config)
-        QATestMessger.start()
-        //
-    }
     //
     def DeployToProd() {
         //
-        this.config.tips = '该服务是否可以上线?'
-        this.config.timeout = 24
-        this.config.timeoutUnit = 'HOURS'
-        def DeployProdMessger = new ConfirmMessgerStage(this.script,'确认上线',this.config)
-        DeployProdMessger.start()
-        //
+        if (this.script.env.CHANGE_TARGET != "DEPLOY_PROD"){
+        //如果是直接部署生产则跳过提示
+            this.config.tips = '该服务是否可以上线?'
+            this.config.timeout = 24
+            this.config.timeoutUnit = 'HOURS'
+            def DeployProdMessger = new ConfirmMessgerStage(this.script,'确认上线',this.config)
+            DeployProdMessger.start()
+        }
         def DeployProd = new DeployProdStage(this.script,'部署测试3服务器',this.config)
         DeployProd.start()
+
+        if (this.script.env.CHANGE_TARGET != "DEPLOY_PROD"){
+        //如果是直接部署生产则跳过提示
+            def SmokeTesting = new SmokeTestingStage(this.script,'冒烟测试',this.config)
+            SmokeTesting.start()
+            }
         //
-        def SmokeTesting = new SmokeTestingStage(this.script,'冒烟测试',this.config)
-        SmokeTesting.start()
-        //
-    }
+        }
 
     def Follow(){
 
@@ -177,6 +206,15 @@ class Tasks implements Serializable {
         def AutoMerge = new PostDeployAutoMergeStage(this.script,'自动生成Changelog',this.config)
         AutoMerge.start()
 
+        }
+    def Rebase(){
+
+        def PreDeployShare = new PreDeployShareStage(this.script,'预部署依赖仓库',this.config);
+        PreDeployShare.start();
+
+        def BuildBaseImage = new BuildBaseImageStage(this.script,'生成基础镜像',this.config)
+        BuildBaseImage.start()
+        }
     }
 }
 
@@ -220,7 +258,8 @@ def call(body) {
     			case 'master':
 	    			// def runTask = new Tasks(this,config);
 	    			runTask.BuildTest()
-	    			runTask.DeployToTest(true);  //跳过部署测试环境
+	    			runTask.DeployToTest1(true);  //跳过部署测试环境
+                    runTask.DeployToTest2(true);  //跳过部署测试环境
 	    			runTask.DeployToTest3()
 	    			runTask.DeployToProd()
                     runTask.Follow()
@@ -229,7 +268,8 @@ def call(body) {
     			case 'develop':
     				// def runTask = new Tasks(this,config);
 	    			runTask.BuildTest()
-	    			runTask.DeployToTest()
+	    			runTask.DeployToTest1()
+                    runTask.DeployToTest2()
 	    			runTask.DeployToTest3()
 	    			runTask.DeployToProd()
                     runTask.Follow()
@@ -243,7 +283,9 @@ def call(body) {
     	case 'FIX_FLOW':
     		// def runTask = new Tasks(this,config);
 	    	runTask.BuildTest()
-	    	runTask.DeployToTest(true);  //跳过部署测试环境
+
+	    	runTask.DeployToTest1(true);  //跳过部署测试环境
+            runTask.DeployToTest2(true);  //跳过部署测试环境
 	    	runTask.DeployToTest3()
 	    	runTask.DeployToProd()
     		break;
@@ -251,10 +293,27 @@ def call(body) {
    		case 'WHOLE_FLOW':
    			// def runTask = new Tasks(this,config);
 	    	runTask.BuildTest(true)
-	    	runTask.DeployToTest(false)
+	    	runTask.DeployToTest1(false)
+            runTask.DeployToTest2(false)
 	    	runTask.DeployToTest3()
 	    	runTask.DeployToProd()
     		break;
+
+        case 'DEPLOY_TEST':
+            runTask.DeployToTest1()
+            break
+        case 'DEPLOY_TEST2':
+            runTask.DeployToTest2()
+            break
+        case 'DEPLOY_TEST3':
+            runTask.DeployToTest3()
+            break
+        case 'DEPLOY_PROD':
+            runTask.DeployToProd()
+            break
+        case 'REBASE':
+            runTask.Rebase()
+            break
     	default:
     		sh "echo 动作匹配失败"
     		sh "exit 1"
