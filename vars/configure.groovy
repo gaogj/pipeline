0f9e7f3cf33822ae7e651fc8dd532828225ead6e
test <test>
2018-09-20 13:44:56 +0800

    test new jenkinsfile

--- vars/configure.groovy
+++ vars/configure.groovy
@@ -5,7 +5,7 @@
 
 // See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
 import cn.kuick.pipeline.Tasks
-
+import cn.kuick.pipeline.stage.*
 // The call(body) method in any file in workflowLibs.git/vars is exposed as a
 // method with the same name as the file.
 def call(body) {
