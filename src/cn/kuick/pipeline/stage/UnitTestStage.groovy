b33f381d48239cfb6b5a635f1c6891f31c69c2b5
test <test>
2018-09-25 14:54:42 +0800

    add unit test

--- src/cn/kuick/pipeline/stage/UnitTestStage.groovy
+++ src/cn/kuick/pipeline/stage/UnitTestStage.groovy
@@ -1,16 +1,19 @@
+/**
+ *	单元测试
+ */
+
 package cn.kuick.pipeline.stage;
 
 import java.io.Serializable;
 
-/**
- *	单元测试
- */
 class UnitTestStage implements Serializable {
 	def script;
 
 	def stageName;
 	def serverName;
 	def version;
+	def config;
+
 
 	UnitTestStage(script, stageName, config) {
 		this.script = script;
@@ -18,6 +21,8 @@ class UnitTestStage implements Serializable {
 		this.stageName = stageName;
 		this.serverName = config.name;
 		this.version = config.version;
+		this.config = config
+
 	}
 
 	def start() {
@@ -34,13 +39,22 @@ class UnitTestStage implements Serializable {
 		def version = this.version;
 		def docker = this.script.docker;
 
-		// We are pushing to a private secure Docker registry in this demo.
-		// 'docker-registry-login' is the username/password credentials ID as defined in Jenkins Credentials.
-		// This is used to authenticate the Docker client to the registry.
-		docker.withRegistry('https://registry.kuick.cn', 'kuick_docker_registry_login') {
-			// 单元测试
-			
-			this.script.echo "Unit Test success!";
+		if (this.config.useUnitTest) {
+			docker.withRegistry('https://registry.kuick.cn', 'kuick_docker_registry_login') {
+				// 覆盖率测试
+				this.script.sh "./shared/scripts/unitTest.sh";
+				this.script.junit 'build/test-results/test/*.xml'
+				
+				// 检查单元测试结果,不匹配则标记job不稳定
+				this.script.jacoco changeBuildStatus: true, maximumLineCoverage: '90',minimumLineCoverage: '89'
+				// 如果pipeline被标记不稳定则终止构建
+				if (this.script.currentBuild.result != 'SUCCESS') {
+					this.script.echo "单元测试未通过" 
+					this.script.sh "exit 1"
+				}
+			}
+		}else{
+			this.script.echo "skip unitTest"
 		}
 	}
 }
