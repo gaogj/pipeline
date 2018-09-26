3526e64c251b1633f17c204b188356f8d84a8350
zhoubangjun <zhoubangjun@kuick.cn>
2018-09-26 17:36:26 +0800

    修复git tag比imags的tag多个字符串‘v'的问题，并修正不存在tag时的错误

--- vars/parseCommitId.groovy
+++ vars/parseCommitId.groovy
@@ -8,7 +8,7 @@ def call() {
 
 		// def lastTagId = sh(returnStdout: true, script: 'git tag --sort=committerdate | tail -1').trim()
 		// jenkins 不支持 --sort=committerdate
-		def lastTagId = sh(returnStdout: true, script: 'git describe --tags `git rev-list --tags --max-count=1`  2>/dev/null || echo noTags').trim()
+		def lastTagId = sh(returnStdout: true, script: '[ "`git tag | head -1`" != "" ] && git describe --tags `git rev-list --tags --max-count=1` | sed "s/^v//" || echo noTags').trim()
 
 		env.COMMIT_ID = shortCommit;
 		env.LAST_TAG_ID = lastTagId
