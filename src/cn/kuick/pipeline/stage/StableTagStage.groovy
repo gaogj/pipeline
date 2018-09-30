a382e2e50998889f730e8e9b50d6c26b829de68f
test <test>
2018-09-30 11:31:59 +0800

    抽取稳定版本镜像

---
+++ src/cn/kuick/pipeline/stage/StableTagStage.groovy
@@ -0,0 +1,47 @@
+package cn.kuick.pipeline.stage;
+
+import java.io.Serializable;
+
+/**
+ *	冒烟测试 + 稳定标签
+ */
+class SmokeTestingStage implements Serializable {
+	def script;
+
+	def stageName;
+	def serverName;
+	def version;
+	def commitId;
+
+	SmokeTestingStage(script, stageName, config) {
+		this.script = script;
+
+		this.stageName = stageName;
+		this.serverName = config.name;
+		this.version = config.version;
+		this.commitId = version[-6..-1];
+	}
+
+	def start() {
+		this.script.stage(this.stageName) {
+		    this.script.node('aliyun345-build') {
+		       	this.run();
+		    }
+		}
+	}
+
+	def run() {
+		def stable_version = "stable";
+		def version = this.version;
+		def docker = this.script.docker;
+
+		// We are pushing to a private secure Docker registry in this demo.
+		// 'docker-registry-login' is the username/password credentials ID as defined in Jenkins Credentials.
+		// This is used to authenticate the Docker client to the registry.
+		docker.withRegistry('https://registry.kuick.cn', 'kuick_docker_registry_login') {
+			// 提供一个构建镜像稳定版本
+			imageName = 'registry.kuick.cn' + '/cc/' + 'this.serverName' + '-server' + ':' + this.version
+			docker.image(imageName).push('stable_version')
+		}
+	}
+}
