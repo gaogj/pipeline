1fce906691ceb21a912693a6749103d81ecbbf28
zhoubangjun <zhoubangjun@kuick.cn>
2018-10-22 13:07:56 +0800

    test

--- src/cn/kuick/pipeline/stage/BuildBaseImageStage.groovy
+++ src/cn/kuick/pipeline/stage/BuildBaseImageStage.groovy
@@ -83,14 +83,14 @@ class BuildBaseImageStage implements Serializable {
 			this.script.sh "clair-scanner ${parameter}"
 
 			this.script.echo "start send success mail!"
+			this.script.emailext attachmentsPattern: reportPath, body: "附件为镜像漏洞扫描结果 At ${imageName}", mimeType: 'text/plain', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: "${name}-server 镜像扫描结果 At buildId(#${buildId})", to: 'zhoubangjun@kuick.cn'
 
-			this.script.emailext attachmentsPattern: reportPath, 
-					body: "附件为镜像漏洞扫描结果 At ${imageName}", 
-					recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], 
-					subject: "${name}-server 镜像扫描结果 At buildId(#${buildId})",
-					
+			//this.script.emailext attachmentsPattern: reportPath, 
+			//		body: "附件为镜像漏洞扫描结果 At ${imageName}", 
+			//		recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], 
+			//		subject: "${name}-server 镜像扫描结果 At buildId(#${buildId})",
+			//		to: 'zhoubangjun@kuick.cn'
 				//	to: toMail,
-					to: 'zhoubangjun@kuick.cn',
 				//	cc: 'devops@kuick.cn'
 
 			this.script.echo "success mail send ok!"
@@ -99,13 +99,13 @@ class BuildBaseImageStage implements Serializable {
 
 			this.script.echo "start send fail mail!"
 
-			this.script.emailext attachmentsPattern: reportPath, 
-					body: "${imageName} 镜像漏洞扫描失败", 
-					recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], 
-					subject: "${name}-server 镜像漏洞扫描失败 At buildId(#${buildId})",
-			
+			this.script.emailext body: "${imageName} 镜像漏洞扫描失败", mimeType: 'text/plain', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: "${name}-server 镜像漏洞扫描失败 At buildId(#${buildId})", to: 'zhoubangjun@kuick.cn'
+
+
+		//	this.script.emailext attachmentsPattern: reportPath, body: "${imageName} 镜像漏洞扫描失败", recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], 
+		//			subject: "${name}-server 镜像漏洞扫描失败 At buildId(#${buildId})",
+		//			to: 'zhoubangjun@kuick.cn'
 		//	to: toMail, 
-					to: 'zhoubangjun@kuick.cn',
 		//	cc: 'devops@kuick.cn'
 	    
 			this.script.echo "fail mail send ok!"
