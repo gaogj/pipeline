0a8da9589aa9f742fc140400c15bf5f416677414
test <test>
2018-10-22 21:00:02 +0800

    测试 api 接口test :fix bug3

--- src/cn/kuick/pipeline/stage/Test3ApiTestingStage.groovy
+++ src/cn/kuick/pipeline/stage/Test3ApiTestingStage.groovy
@@ -60,7 +60,7 @@ class Test3ApiTestingStage implements Serializable {
 
 	    try {
 	    	// Create a lock file
-	    	writeFile file: lockFile, text: "${this.script.env.BUILD_ID} of ${this.script.env.JOB_NAME}"
+	    	this.script.writeFile file: lockFile, text: "${this.script.env.JOB_NAME} of ${this.script.env.BUILD_ID}"
 	    	this.script.sh "cd api-test && sh run.sh "
 	    }
 	    catch(Exception e) {
