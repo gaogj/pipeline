5078377136206124e80aaff54286a7714b2f3f1d
test <test>
2018-10-23 10:17:06 +0800

    优化pipeline

--- src/cn/kuick/pipeline/stage/ConfirmMessgerStage.groovy
+++ src/cn/kuick/pipeline/stage/ConfirmMessgerStage.groovy
@@ -13,7 +13,7 @@ class ConfirmMessgerStage implements Serializable {
 	def timeoutUnit
 
 	def stageName
-	def tips;
+	def tips
 
 	ConfirmMessgerStage(script, stageName, tips, config) {
 		this.config = config
@@ -25,7 +25,7 @@ class ConfirmMessgerStage implements Serializable {
 
 	def start() {
 		this.script.stage(this.stageName) {
-		    this.run();
+		    this.run()
 		}
 	}
 
