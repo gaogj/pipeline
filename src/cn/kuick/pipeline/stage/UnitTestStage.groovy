7633bfd297212378923738c728e996d932ff720d
test <test>
2018-10-23 09:58:24 +0800

    优化pipeline,

--- src/cn/kuick/pipeline/stage/UnitTestStage.groovy
+++ src/cn/kuick/pipeline/stage/UnitTestStage.groovy
@@ -26,34 +26,36 @@ class UnitTestStage implements Serializable {
 	}
 
 	def start() {
-		this.script.stage(this.stageName) {
-		    this.script.node('aliyun345-build') {
-		    	this.script.checkout this.script.scm
-
-		       	this.run();
-		    }
+		if (this.config.useUnitTest) {
+			this.script.stage(this.stageName) {
+			    this.script.node('aliyun345-build') {
+			    	this.script.echo "login to aliyun345-build"
+			    	this.script.checkout this.script.scm
+			       	this.run();
+			    }
+			}
+		}else{
+			this.script.stage(this.stageName) {
+			this.script.echo "skip unitTest"
+			}
 		}
 	}
 
 	def run() {
 		def version = this.version;
 		def docker = this.script.docker;
-
-		if (this.config.useUnitTest) {
-			docker.withRegistry('https://registry.kuick.cn', 'kuick_docker_registry_login') {
-				// 覆盖率测试
-				this.script.sh "pwd && ./shared/scripts/unitTest.sh";
-				this.script.junit 'build/test-results/test/*.xml'
-				// 检查单元测试结果,不匹配则标记job不稳定
-				this.script.jacoco changeBuildStatus: true, maximumLineCoverage: '80',minimumLineCoverage: '70'
-				// 如果pipeline被标记不稳定则终止构建
-				if (this.script.currentBuild.result != 'SUCCESS') {
-					this.script.echo "单元测试未通过" 
-					this.script.sh "exit 1"
-				}
+		
+		docker.withRegistry('https://registry.kuick.cn', 'kuick_docker_registry_login') {
+			// 覆盖率测试
+			this.script.sh "./shared/scripts/unitTest.sh";
+			this.script.junit 'build/test-results/test/*.xml'
+			// 检查单元测试结果,不匹配则标记job不稳定
+			this.script.jacoco changeBuildStatus: true, maximumLineCoverage: '80',minimumLineCoverage: '70'
+			// 如果pipeline被标记不稳定则终止构建
+			if (this.script.currentBuild.result != 'SUCCESS') {
+				this.script.echo "单元测试未通过" 
+				this.script.sh "exit 1"
 			}
-		}else{
-			this.script.echo "skip unitTest"
 		}
 	}
 }
